fb8aa9ac3be40dc94b83f8614333567ea6da09a0
Чабанов Сергей <chabanovsn@gmail.com>
2026-03-10 21:58:44 +0300

    dz3

---
+++ 5-languages/app.js
@@ -0,0 +1,14 @@
+const lang = 'de';
+switch  (lang) { 
+    case 'de':
+        console.log('Gutten tag!');
+        break;
+    case 'ru':
+        console.log('Добрый день!');
+        break;
+    case 'en':
+        console.log('Hello!');
+        break;
+    default:
+         console.log('How are you ?');
+   }
