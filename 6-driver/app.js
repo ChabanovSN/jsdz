52fb46577a9d174d6b0a3f178d65b56b436f5d46
Чабанов Сергей <chabanovsn@gmail.com>
2026-03-18 16:50:58 +0300

    dz4.1

--- 6-driver/app.js
+++ 6-driver/app.js
@@ -2,4 +2,4 @@ const hasLicence = true;
 const age = 18;
 let isDrunk = false;
 const canDrive = age >= 18 && hasLicence && !isDrunk;
-console.log(`Он  ${canDrive ? 'можит':' не можит'} вести машину.`);
+console.log(`Он  ${canDrive ? 'может':' не может'} вести машину.`);
